9155b77654259b845a51f75a6a963ee97b6a28b1
Katie Rose <rose_katherine@bah.com>
2016-05-23 13:38:41 -0400

    Adding deployment script.

---
+++ deploy.py
@@ -0,0 +1,83 @@
+import boto
+from boto import ec2
+import sh
+import os
+import json
+
+def deploy():
+
+	# set packer path
+	packer_file = '/var/lib/jenkins/hosts/packer.json'
+	packer_exec_path = '/packer/packerio'
+
+	# Set connection
+	print('Connecting to AWS via region us-gov-west-1...')
+	conn = boto.ec2.connect_to_region(region_name='us-gov-west-1')
+	print('Done.')
+
+	# Retrieve current App Instance AMIs
+	print('Retrieving current app instance AMI(s)...')
+	current_app_amis = conn.get_all_images(filters={"tag:current" : "True", "tag:base" : "False", "tag:type" : "Application"})
+	print('Done. Current app instance AMI(s): '+ '\n'.join(map(str, current_app_amis)) )
+
+	# Retrieve current Base app AMI (where type=app and current=true)
+	print('Retrieving current base app AMI...')
+	current_base_ami = conn.get_all_images(filters={"tag:current" : "True", "tag:base" : "True", "tag:type" : "Application"})[0].id
+	print('Done. Current base app AMI: '+current_base_ami)
+	
+	# Insert current Base app AMI into packer file 
+	print('Updating Packer file with current base app AMI...')
+	update_packer_spec(packer_file, current_base_ami)
+	print('Done.')
+
+	# Build new app instance AMI via Packer
+	print('Buiding new app instance AMI via Packer. This may take a few minutes...')
+	packer_output = packer_build(packer_file, packer_exec_path)
+	ami_line = [line for line in packer_output.split('\n') if "amazon-ebs: AMIs were created:" in line][0]
+	ami_id = ami_line[ami_line.find('ami-'):ami_line.find('ami-')+12]
+	print('Done. Packer AMI created: '+ami_id)
+
+	# Set current=False tag for old App AMIs
+	print('Setting current tag to False on old app instance AMIs')
+	update_ami_tags(current_app_amis)
+	print('Done')
+
+	# Confirm app instance that was created is now only current=True tagged AMI
+	if ami_id == conn.get_all_images(filters={"tag:current" : "True", "tag:base" : "False", "tag:type" : "Application"})[0].id:
+		print('Success! Packer AMI id matches current tagged AMI.')
+	else:
+		print('Something went wrong. Packer AMI: '+ami_id+'; Tagged AMI: '+conn.get_all_images(filters={"tag:current" : "True", "tag:base" : "False", "tag:type" : "Application"})[0].id)
+
+	#Add new AMI id to terraform variables
+
+	# Run terraform
+
+def packer_build(packer_file='packer.json', packer_exec_path='packer'):
+	cmd = sh.Command(packer_exec_path).build.bake(packer_file).bake('-machine-readable')
+	return cmd()
+
+
+def update_packer_spec(packer_file='packer.json', current_base_ami=''):
+	packer_json = open(packer_file, "r")
+	packer_data = json.load(packer_json)
+	packer_json.close()
+
+	packer_data['builders'][0]['source_ami'] = current_base_ami
+
+	packer_json = open(packer_file, "w+")
+	packer_json.write(json.dumps(packer_data))
+	packer_json.close()
+
+	return
+
+
+def update_ami_tags(current_app_amis=False):
+	for ami in current_app_amis:
+		ami.remove_tag('current','True')
+		ami.add_tag('current','False')
+
+	return
+
+
+if __name__ == '__main__':
+	deploy()
